fbb7b81ee3a34ab37688114091fa53cbc8bfb894
Berat Mert Çiğdem <103438777+Mertovic24@users.noreply.github.com>
2024-07-15 19:47:53 +0300

    Update blackjack.js

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,13 +1,9 @@
-
 let dealerSum = 0;
 let yourSum = 0;
-
 let dealerAceCount = 0;
 let yourAceCount = 0; 
-
 let hidden;
 let deck;
-
 let canHit = true; //allows the player (you) to draw while yourSum <= 21
 
 window.onload = function() {
